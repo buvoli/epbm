71e882e091201f20ddbb053fa49e6692609f2f7d
buvoli <buvoli@uw.edu>
2020-11-05 16:20:04 -0800

    removed unecessary files

--- partitioned/.cjr/clear-sd/build-scripts/user_install_extra.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# -- User INSTALL EXTRA SCRIPT -------------------------------------------------
-# Use this shell script for adding any additional dependancies.
-# NOTE: This shell script will be run by a standard user.
-# ------------------------------------------------------------------------------
--- partitioned/.cjr/clear/build-scripts/root_install_extra.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# -- ROOT INSTALL EXTRA SCRIPT -------------------------------------------------
-# Use this shell script for addint any additionaly dependancies.
-#
-#    E.G. dnf install <missing package>
-#
-# NOTE: This shell script will be run by root.
-# ------------------------------------------------------------------------------
